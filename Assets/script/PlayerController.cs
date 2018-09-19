f0686572a59ba133305d05f3a4525707adc618a9
vettsm23 <42977979+vettsm23@users.noreply.github.com>
2018-09-19 13:43:18 -0400

    Added project

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text wintext;
+
+    private Rigidbody rb;
+    private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        wintext.text = "";
+    }
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 13)
+        {
+            wintext.text = "You win!";
+        }
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+  
+}
+
